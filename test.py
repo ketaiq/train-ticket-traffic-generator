54c20a5a52598f872d28252507f2dbddb7c9db2f
Qiu Ketai <ketai.qiu@outlook.com>
2022-06-08 22:08:19 +0200

    Add and test requests for station service

--- test.py
+++ test.py
@@ -1,4 +1,69 @@
-from ts.services.contacts_service import gen_random_contact
 import uuid
+import unittest
 
-print(gen_random_contact(str(uuid.uuid4()), str(uuid.uuid4())).__dict__)
+
+class ServiceRequestTestCase(unittest.TestCase):
+    def test_contacts_service(self):
+        from ts.services.contacts_service import gen_random_contact
+
+        print("\n\nTest contacts_service")
+        print("Test gen_random_contact")
+        print(gen_random_contact(str(uuid.uuid4()), str(uuid.uuid4())).__dict__)
+
+    def test_auth_service(self):
+        from ts.services.auth_service import login_user_request
+
+        print("\n\nTest auth_service")
+        print("Test login_user_request")
+        request_id = str(uuid.uuid4())
+        admin_bearer, user_id = login_user_request(
+            username="admin", password="222222", request_id=request_id
+        )
+        print(admin_bearer)
+        print(user_id)
+        self.assertIsInstance(admin_bearer, str)
+        self.assertIsInstance(user_id, str)
+
+    def test_station_service(self):
+        from ts.services.auth_service import login_user_request
+        from ts.services.station_service import (
+            get_all_stations_request,
+            add_one_new_station_request,
+            update_one_station_request,
+            delete_one_station_request,
+        )
+
+        print("\n\nTest station_service")
+        print("Test get_all_stations_request")
+        request_id = str(uuid.uuid4())
+        admin_bearer, admin_user_id = login_user_request(
+            username="admin", password="222222", request_id=request_id
+        )
+        stations = get_all_stations_request(admin_user_id, admin_bearer)
+        print(stations)
+        self.assertIsInstance(stations, list)
+
+        print("\nTest add_one_new_station_request")
+        added_station = add_one_new_station_request(
+            admin_bearer, admin_user_id, "Lugano", 5
+        )
+        print(added_station)
+        self.assertIsInstance(added_station, dict)
+
+        print("\nTest update_one_station_request")
+        updated_station = update_one_station_request(
+            admin_bearer, admin_user_id, "lugano", "New Lugano", 10
+        )
+        print(updated_station)
+        self.assertIsInstance(updated_station, dict)
+
+        print("\nTest delete_one_station_request")
+        deleted_station = delete_one_station_request(
+            admin_bearer, admin_user_id, "lugano", "New Lugano"
+        )
+        print(deleted_station)
+        self.assertIsInstance(deleted_station, dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- ts/services/station_service.py
+++ ts/services/station_service.py
@@ -3,11 +3,17 @@ This module includes all API calls provided by ts-station-service.
 """
 
 import logging
-from locust.clients import HttpSession
 import requests
+from ts.log_syntax.locust_response import (
+    log_wrong_response_warning,
+    log_timeout_warning,
+    log_response_info,
+)
+from json import JSONDecodeError
 
 
-def get_all_stations(client: HttpSession, admin_bearer: str, request_id: str):
+def get_all_stations(client, admin_bearer: str, admin_user_id: str):
+    operation = "get all stations"
     with client.get(
         url="/api/v1/stationservice/stations",
         headers={
@@ -18,22 +24,45 @@ def get_all_stations(client: HttpSession, admin_bearer: str, request_id: str):
         name="get all stations",
     ) as response:
         if response.json()["msg"] != "Find all content":
-            log = f"request {request_id} tries to get all train stations but gets wrong response"
-            response.failure(log)
-            logging.error(f"{log} {response.json()}")
+            log_wrong_response_warning(admin_user_id, operation, response)
         elif response.elapsed.total_seconds() > 10:
-            log = f"request {request_id} tries to get all train stations but request takes too long!"
-            response.failure(log)
-            logging.warning(log)
+            log_timeout_warning(admin_user_id, operation, response)
         else:
             stations = response.json()["data"]
-            logging.info(f"request {request_id} gets all train stations {stations}")
+            log_response_info(admin_user_id, operation, stations)
+
+
+def get_all_stations_request(admin_user_id: str, admin_bearer: str) -> list:
+    operation = "get food menu"
+    r = requests.get(
+        url="http://35.238.101.76:8080/api/v1/stationservice/stations",
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": admin_bearer,
+        },
+    )
+    try:
+        key = "msg"
+        msg = r.json()["msg"]
+        if msg != "Find all content":
+            print(
+                f"user {admin_user_id} tries to {operation} but gets wrong response {msg}"
+            )
+        else:
+            key = "data"
+            stations = r.json()["data"]
+            return stations
+    except JSONDecodeError:
+        print("Response could not be decoded as JSON")
+    except KeyError:
+        print(f"Response did not contain expected key '{key}'")
 
 
 def add_one_new_station(
-    client: HttpSession,
+    client,
     admin_bearer: str,
-    request_id: str,
+    admin_user_id: str,
     new_station_name: str,
     stay_time: int,
 ):
@@ -53,30 +82,23 @@ def add_one_new_station(
         name=operation,
     ) as response:
         if response.json()["msg"] != "Create success":
-            log = f"request {request_id} tries to {operation} but gets wrong response"
-            logging.error(f"log {response.json()}")
-            response.failure(log)
+            log_wrong_response_warning(admin_user_id, operation, response)
         elif response.elapsed.total_seconds() > 10:
-            log = (
-                f"request {request_id} tries to {operation} but request takes too long!"
-            )
-            logging.warning(log)
-            response.failure(log)
+            log_timeout_warning(admin_user_id, operation, response)
         else:
             new_station = response.json()["data"]
-            logging.info(f"request {request_id} {operation} {new_station}")
+            log_response_info(admin_user_id, operation, new_station)
 
 
-def update_one_station(
-    client: HttpSession,
+def add_one_new_station_request(
     admin_bearer: str,
-    request_id: str,
+    admin_user_id: str,
     new_station_name: str,
     stay_time: int,
-):
-    operation = "update a station"
-    with client.put(
-        url="/api/v1/stationservice/stations",
+) -> dict:
+    operation = "add a new station"
+    r = requests.post(
+        url="http://35.238.101.76:8080/api/v1/stationservice/stations",
         headers={
             "Accept": "application/json",
             "Content-Type": "application/json",
@@ -87,28 +109,100 @@ def update_one_station(
             "name": new_station_name,
             "stayTime": stay_time,
         },
+    )
+    try:
+        key = "msg"
+        msg = r.json()["msg"]
+        if msg != "Create success":
+            print(
+                f"user {admin_user_id} tries to {operation} but gets wrong response {msg}"
+            )
+        else:
+            key = "data"
+            new_station = r.json()["data"]
+            return new_station
+    except JSONDecodeError:
+        print("Response could not be decoded as JSON")
+    except KeyError:
+        print(f"Response did not contain expected key '{key}'")
+
+
+def update_one_station(
+    client,
+    admin_bearer: str,
+    admin_user_id: str,
+    station_id: str,
+    new_station_name: str,
+    new_station_stay_time: int,
+):
+    operation = "update a station"
+    with client.put(
+        url="/api/v1/stationservice/stations",
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": admin_bearer,
+        },
+        json={
+            "id": station_id,
+            "name": new_station_name,
+            "stayTime": new_station_stay_time,
+        },
         name=operation,
     ) as response:
         if response.json()["msg"] != "Update success":
-            log = f"request {request_id} tries to {operation} but gets wrong response"
-            logging.error(f"log {response.json()}")
-            response.failure(log)
+            log_wrong_response_warning(admin_user_id, operation, response)
         elif response.elapsed.total_seconds() > 10:
-            log = (
-                f"request {request_id} tries to {operation} but request takes too long!"
-            )
-            logging.warning(log)
-            response.failure(log)
+            log_timeout_warning(admin_user_id, operation, response)
         else:
-            station = response.json()["data"]
-            logging.info(f"request {request_id} {operation} {station}")
+            updated_station = response.json()["data"]
+            log_response_info(admin_user_id, operation, updated_station)
+
+
+def update_one_station_request(
+    admin_bearer: str,
+    admin_user_id: str,
+    station_id: str,
+    new_station_name: str,
+    new_station_stay_time: int,
+) -> dict:
+    operation = "update a station"
+    r = requests.put(
+        url="http://35.238.101.76:8080/api/v1/stationservice/stations",
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": admin_bearer,
+        },
+        json={
+            "id": station_id,
+            "name": new_station_name,
+            "stayTime": new_station_stay_time,
+        },
+    )
+    try:
+        key = "msg"
+        msg = r.json()["msg"]
+        if msg != "Update success":
+            print(
+                f"user {admin_user_id} tries to {operation} but gets wrong response {msg}"
+            )
+        else:
+            key = "data"
+            updated_station = r.json()["data"]
+            return updated_station
+    except JSONDecodeError:
+        print("Response could not be decoded as JSON")
+    except KeyError:
+        print(f"Response did not contain expected key '{key}'")
 
 
 def delete_one_station(
-    client: HttpSession,
+    client,
     admin_bearer: str,
-    request_id: str,
-    new_station_name: str,
+    admin_user_id: str,
+    station_id: str,
+    station_name: str,
 ):
     operation = "delete a station"
     with client.delete(
@@ -119,21 +213,51 @@ def delete_one_station(
             "Authorization": admin_bearer,
         },
         json={
-            "id": new_station_name.lower(),
-            "name": new_station_name,
+            "id": station_id,
+            "name": station_name,
         },
         name=operation,
     ) as response:
         if response.json()["msg"] != "Delete success":
-            log = f"request {request_id} tries to {operation} but gets wrong response"
-            logging.error(f"log {response.json()}")
-            response.failure(log)
+            log_wrong_response_warning(admin_user_id, operation, response)
         elif response.elapsed.total_seconds() > 10:
-            log = (
-                f"request {request_id} tries to {operation} but request takes too long!"
-            )
-            logging.warning(log)
-            response.failure(log)
+            log_timeout_warning(admin_user_id, operation, response)
         else:
-            station = response.json()["data"]
-            logging.info(f"request {request_id} {operation} {station}")
+            deleted_station = response.json()["data"]
+            log_response_info(admin_user_id, operation, deleted_station)
+
+
+def delete_one_station_request(
+    admin_bearer: str,
+    admin_user_id: str,
+    station_id: str,
+    station_name: str,
+) -> dict:
+    operation = "update a station"
+    r = requests.delete(
+        url="http://35.238.101.76:8080/api/v1/stationservice/stations",
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": admin_bearer,
+        },
+        json={
+            "id": station_id,
+            "name": station_name,
+        },
+    )
+    try:
+        key = "msg"
+        msg = r.json()["msg"]
+        if msg != "Delete success":
+            print(
+                f"user {admin_user_id} tries to {operation} but gets wrong response {msg}"
+            )
+        else:
+            key = "data"
+            deleted_station = r.json()["data"]
+            return deleted_station
+    except JSONDecodeError:
+        print("Response could not be decoded as JSON")
+    except KeyError:
+        print(f"Response did not contain expected key '{key}'")
